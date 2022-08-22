61e2d8e37a37f0884263c0f36db408729a5d13ec
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2022-08-22 12:02:09 -0400

    Sort licenses by full name in GitHub license helper

--- scraper/github/util.py
+++ scraper/github/util.py
@@ -11,8 +11,11 @@ def _license_obj(license):
     """
     obj = None
 
-    if license in ("MIT", "MIT License"):
-        obj = {"URL": "https://api.github.com/licenses/mit", "name": "MIT"}
+    if license in ("Apache License 2.0"):
+        obj = {
+            "URL": "https://api.github.com/licenses/apache-2.0",
+            "name": "Apache-2.0",
+        }
     elif license in ('BSD 2-clause "Simplified" License'):
         obj = {
             "URL": "https://api.github.com/licenses/bsd-2-clause",
@@ -23,31 +26,28 @@ def _license_obj(license):
             "URL": "https://api.github.com/licenses/bsd-3-clause",
             "name": "BSD-3-Clause",
         }
-    elif license in ("Apache License 2.0"):
-        obj = {
-            "URL": "https://api.github.com/licenses/apache-2.0",
-            "name": "Apache-2.0",
-        }
-    elif license in ("GNU General Public License v2.1"):
-        obj = {"URL": "https://api.github.com/licenses/gpl-2.1", "name": "GPL-2.1"}
-    elif license in ("GNU General Public License v2.0"):
-        obj = {"URL": "https://api.github.com/licenses/gpl-2.0", "name": "GPL-2.0"}
-    elif license in ("GNU Lesser General Public License v2.1"):
-        obj = {"URL": "https://api.github.com/licenses/lgpl-2.1", "name": "LGPL-2.1"}
-    elif license in ("GNU General Public License v3.0"):
-        obj = {"URL": "https://api.github.com/licenses/gpl-3.0", "name": "GPL-3.0"}
-    elif license in ("GNU Lesser General Public License v3.0"):
-        obj = {"URL": "https://api.github.com/licenses/lgpl-3.0", "name": "LGPL-3.0"}
     elif license in ("Eclipse Public License 1.0"):
         obj = {"URL": "https://api.github.com/licenses/epl-1.0", "name": "EPL-1.0"}
+    elif license in ("Eclipse Public License 2.0"):
+        obj = {"URL": "https://api.github.com/licenses/epl-2.0", "name": "EPL-2.0"}
+    elif license in ("GNU Affero General Public License v3.0"):
+        obj = {"URL": "https://api.github.com/licenses/agpl-3.0", "name": "AGPL-3.0"}
+    elif license in ("GNU General Public License v2.0"):
+        obj = {"URL": "https://api.github.com/licenses/gpl-2.0", "name": "GPL-2.0"}
+    elif license in ("GNU General Public License v2.1"):
+        obj = {"URL": "https://api.github.com/licenses/gpl-2.1", "name": "GPL-2.1"}
+    elif license in ("GNU General Public License v3.0"):
+        obj = {"URL": "https://api.github.com/licenses/gpl-3.0", "name": "GPL-3.0"}
+    elif license in ("GNU Lesser General Public License v2.1"):
+        obj = {"URL": "https://api.github.com/licenses/lgpl-2.1", "name": "LGPL-2.1"}
+    elif license in ("GNU Lesser General Public License v3.0"):
+        obj = {"URL": "https://api.github.com/licenses/lgpl-3.0", "name": "LGPL-3.0"}
+    elif license in ("MIT", "MIT License"):
+        obj = {"URL": "https://api.github.com/licenses/mit", "name": "MIT"}
     elif license in ("Mozilla Public License 2.0"):
         obj = {"URL": "https://api.github.com/licenses/mpl-2.0", "name": "MPL-2.0"}
     elif license in ("The Unlicense"):
         obj = {"URL": "https://api.github.com/licenses/unlicense", "name": "Unlicense"}
-    elif license in ("GNU Affero General Public License v3.0"):
-        obj = {"URL": "https://api.github.com/licenses/agpl-3.0", "name": "AGPL-3.0"}
-    elif license in ("Eclipse Public License 2.0"):
-        obj = {"URL": "https://api.github.com/licenses/epl-2.0", "name": "EPL-2.0"}
 
     if obj is None:
         logger.warn("I don't understand the license: %s", license)
